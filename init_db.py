9b626a13655abc9fc90c0d355e7255dce6eba612
AlexPR <aleksejparhomenko14192@gmail.com>
2025-01-31 20:23:29 +0300

    hello

--- init_db.py
+++ init_db.py
@@ -7,16 +7,20 @@ create_db_query = f"CREATE DATABASE {DB_NAME};"
 
 def create_db_user():
     try:
+        print("НЕТ ОШИБКИ")
         conn = psycopg2.connect(
             host=DB_HOST,
             user=DB_USER,
-            password=DB_PASS
+            password=DB_PASS,
         )
+        print("НЕТ ОШИБКИ 2")
         conn.autocommit = True
         cursor = conn.cursor()
 
+        print("НЕТ ОШИБКИ 3")
         cursor.execute(f"SELECT 1 FROM pg_database WHERE datname = '{DB_NAME}';")
         db_exists = cursor.fetchone()
+        print("НЕТ ОШИБКИ 4")
 
         if not db_exists:
             cursor.execute(create_db_query)
